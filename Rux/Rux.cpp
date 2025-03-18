51401aff6da8c9b4ea0f2a95b8b0b7306182462f
Ivan Muzyka <musicvano@gmail.com>
2025-03-18 11:15:07 +0200

    Initial commit

---
+++ Rux/Rux.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+void ShowHelp()
+{
+	using std::cout;
+	cout << "Usage: rux [options] [commands] input\n";
+	cout << "Options:\n";
+	cout << "   -h, --help      Show help\n";
+	cout << "   -v, --version   Show version\n";
+	cout << "Commands\n";
+	cout << "    build          Compile the current package\n";
+	cout << "    clean          Remove the target directory\n";
+	cout << "    init           Create a new package in the current directory\n";
+	cout << "    new            Create a new package\n";
+	cout << "    run            Run a binary" << std::endl;
+}
+
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1)
+		ShowHelp();
+	return 0;
+}
