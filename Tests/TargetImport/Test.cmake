7d0e57674a438ef1d56c813fa43fcdcece54527d
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-06-09 23:36:47 +0300

    Specify name for TargetImport executable

--- Tests/TargetImport/Test.cmake
+++ Tests/TargetImport/Test.cmake
@@ -5,6 +5,6 @@ message(STATUS "Using rux: ${RUX}")
 # rux_prepare_test runs `rux build`. If collectImports() triggers an error
 # due to the unlisted platform dependencies on the current host,
 # this function intercepts the non-zero status code and fails the test instantly.
-rux_prepare_test("${TEST_DIR}" "${TEST_NAME}" target_binary)
+rux_prepare_test("${TEST_DIR}" "target_import_test" target_binary)
 
 message(STATUS "PASS: ${TEST_NAME}")
